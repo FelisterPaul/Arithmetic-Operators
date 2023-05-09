0ce4eb3deb4b64d51522ca91e591c5cf9301724d
FelisterPaul <61831733+FelisterPaul@users.noreply.github.com>
2023-05-09 04:48:58 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print(10 + 3) #Addition Operators
+
+print(10 / 3) #Division with a floating result
+
+print(10 // 3) #Division with an integer as the result
+
+print(10 % 3) #Modulus = Returns the reminder of the division of 10 by 3
+
+print(10 - 3) #Subtraction arithmetic operator
