5af37fc65621b9c5d8480c14a14c384388975ac6
FelisterPaul <61831733+FelisterPaul@users.noreply.github.com>
2023-05-10 04:03:32 +0000

    argumented operator assignment
    argumented operator assignment

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 print(10 + 3) #Addition Operators
-
 print(10 / 3) #Division with a floating result
-
 print(10 // 3) #Division with an integer as the result
-
 print(10 % 3) #Modulus = Returns the reminder of the division of 10 by 3
-
 print(10 - 3) #Subtraction arithmetic operator
+print(10 ** 3) #Exponentiation operator 
+#Argumented asign operator
+x = 10
+x = x + 3
+print(x)
+#This is displayed below...continuation of argumented operator
+x = 10
+x += 3
+print(x)
